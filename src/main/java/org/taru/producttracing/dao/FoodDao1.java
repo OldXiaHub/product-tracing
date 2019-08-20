b9c730603aff2db298e963acb320d3a0344d035f
Kimtress <570743124@qq.com>
2019-08-20 19:43:53 +0800

    hhhh

---
+++ src/main/java/org/taru/producttracing/dao/FoodDao1.java
@@ -0,0 +1,12 @@
+package org.taru.producttracing.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Mapper
+@Repository
+public interface FoodDao1 {
+    //查询新闻资讯信息
+    List queryNews();
+}
---
+++ src/main/java/org/taru/producttracing/service/FoodService1.java
@@ -0,0 +1,11 @@
+package org.taru.producttracing.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public interface FoodService1 {
+    //查询新闻资讯信息
+    List queryNews();
+
+}
