b41fb82acc4b0d75c4bc4def8b4259c43bd70d26
543198468 <543198468@qq.com>
2019-08-21 14:19:25 +0800

    新闻 wangkaifei 8/21 11：50

---
+++ src/main/java/org/taru/producttracing/service/NewsService.java
@@ -0,0 +1,10 @@
+package org.taru.producttracing.service;
+
+import org.taru.producttracing.pojo.News;
+
+import java.util.List;
+
+public interface NewsService {
+    List<News> queryNews();
+    public int deleteNews(String id);
+}
---
+++ src/main/java/org/taru/producttracing/service/impl/NewsServiceImpl.java
@@ -0,0 +1,24 @@
+package org.taru.producttracing.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.taru.producttracing.dao.NewsDao;
+import org.taru.producttracing.pojo.News;
+import org.taru.producttracing.service.NewsService;
+import java.util.List;
+
+@Service
+public class NewsServiceImpl implements NewsService {
+    @Autowired
+    NewsDao newsDao;
+    @Override
+    public  List<News> queryNews(){
+            return newsDao.selectNews();
+    }
+    @Override
+    public int deleteNews(String id){
+           int news=newsDao.delNewsById(id);
+           return news;
+    }
+
+}
