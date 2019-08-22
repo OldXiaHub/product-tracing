80060c43225d5e54ea072543f23062fd16e70019
Zhangrui-super <1787217893@qq.com>
2019-08-22 17:39:26 +0800

    2019/8/22-----zhangrui17.39

--- src/main/java/org/taru/producttracing/service/Impl/UserServiceByZhangRImpl.java
+++ src/main/java/org/taru/producttracing/service/Impl/UserServiceByZhangRImpl.java
@@ -3,9 +3,12 @@ package org.taru.producttracing.service.Impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.taru.producttracing.dao.UserDaoByZhangR;
+import org.taru.producttracing.pojo.Complain;
 import org.taru.producttracing.pojo.User;
 import org.taru.producttracing.service.UserServiceByZhangR;
 import org.taru.producttracing.util.SecurityUtl;
+
+import java.util.List;
 
 
 @Service
@@ -22,22 +25,49 @@ UserDaoByZhangR userDaoByZhangR;
         User user= userDaoByZhangR.login(username,password);
         return user;
     }
-
     /**
      * autnor:zhangrui
      * time:2019/8/20-17:27
      * 前台用户注册
      */
-    @Override
-    public void register(User user) {
-        //用 MD5生成用户id
-        user.setOpenId(SecurityUtl.getMd5String("xiazhongqiang"));
-        userDaoByZhangR.userRegister(user);
-    }
+
+//    @Override
+//    public void register(User user) {
+//        //用 MD5生成用户id
+//        user.setOpenId(SecurityUtl.getMd5String("xiazhongqiang"));
+//        userDaoByZhangR.userRegister(user);
+//    }
 
     /**
      * 用户个人信息获取
      *author:zhangrui
      * time：2019/08/21-14:17
      */
+    @Override
+    public User getUserInfo(String nickName) {
+        User user = userDaoByZhangR.getUserInfo(nickName);
+        return user;
+    }
+
+    /**
+     * 添加用户个人信息
+     * author:zhangrui
+     * time:2019/8/21-15:37
+     */
+    public int addUserInfo(User nickName){
+        int i = userDaoByZhangR.addUserInfo(nickName);
+        return i;
+    }
+
+    /**
+     * 查询自己的投诉记录
+     * author:zhangrui
+     * time:2019/8/22-9:30
+     */
+
+    @Override
+    public List<Complain> userComplain(String nickName) {
+        List <Complain> list = userDaoByZhangR.userComplain(nickName);
+        return list;
+    }
 }
