acc80e6583ae6650a56bc98844ab69e42f987ad8
zyx <zyx1020d@163.com>
2019-08-22 11:19:51 +0800

    工厂 update

---
+++ src/main/java/org/taru/producttracing/service/Impl/AdminServiceImpl.java
@@ -0,0 +1,67 @@
+package org.taru.producttracing.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.taru.producttracing.dao.UserDao;
+import org.taru.producttracing.pojo.Admin;
+import org.taru.producttracing.pojo.Factory;
+import org.taru.producttracing.service.UserService;
+
+import java.util.List;
+
+@Service
+public class AdminServiceImpl implements UserService {
+
+    @Autowired
+    UserDao userDao;
+
+    /**
+     * 后台登录
+     * @param adminName
+     * @param adminPassword
+     * @return
+     */
+    public Admin login(String adminName, String adminPassword) {
+        Admin admin=userDao.adminLogin(adminName,adminPassword);
+        return admin;
+    }
+
+    /**
+     * 添加工场
+     * @param factory
+     * @return
+     */
+    @Override
+    public void addFactory(Factory factory) {
+        userDao.addFactory(factory);
+    }
+
+    /**
+     * 删除工厂
+     * @param factoryId
+     */
+    @Override
+    public void updateFactory(String factoryId) {
+        userDao.updateFactory(factoryId);
+    }
+
+    /**
+     * 修改工厂信息
+     * @param factory
+     */
+    @Override
+    public void modifyFactory(Factory factory) {
+        userDao.modifyFactory(factory);
+    }
+
+    /**
+     * 查询所有工厂
+     * @return
+     */
+    @Override
+    public List<Factory> findAllFactory() {
+        List<Factory> factories=userDao.findAllFactory();
+        return factories;
+    }
+
+}
