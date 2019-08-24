5c443a2576bc03dfea9bbc291a6a238b7433bd7a
helloDawei <759843425@qq.com>
2019-08-24 15:34:28 +0800

    2019.8.24  徐大伟

--- src/main/java/org/taru/producttracing/util/QRFactory.java
+++ src/main/java/org/taru/producttracing/util/QRFactory.java
@@ -17,7 +17,7 @@ public class QRFactory {
     static BASE64Encoder encoder = new sun.misc.BASE64Encoder();
     public static void creteQRFile(String batchBarcode)
     {
-        String info = String.format("http://127.0.0.1/Api/querybatch?batchId=%s",batchBarcode);
+        String info = String.format("http://127.0.0.1/Api/querybatch?batchBarcode=%s",batchBarcode);
         String destPath = "E:\\QRcodefile\\"+batchBarcode;
         try {
             QRCode.encode(info, null, destPath, true);
@@ -29,11 +29,11 @@ public class QRFactory {
 
     /**
      * 图片转换为字节
-     * @param batchId
+     * @param batchBarcode
      * @return
      */
-    public static String getImageBinary(String batchId) {
-        File f = new File("E:\\QRcodefile\\"+batchId);
+    public static String getImageBinary(String batchBarcode) {
+        File f = new File("E:\\QRcodefile\\"+batchBarcode);
         BufferedImage bi;
         try {
             bi = ImageIO.read(f);
