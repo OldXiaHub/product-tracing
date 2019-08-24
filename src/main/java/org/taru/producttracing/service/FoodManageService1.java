dc32d8c1f98694d9bb4c66031972eb4567247e4e
helloDawei <759843425@qq.com>
2019-08-24 21:53:32 +0800

    2019.8.24 查所有批次 徐大伟

--- src/main/java/org/taru/producttracing/service/FoodManageService1.java
+++ src/main/java/org/taru/producttracing/service/FoodManageService1.java
@@ -2,6 +2,8 @@ package org.taru.producttracing.service;
 
 import org.taru.producttracing.pojo.Batch;
 
+import java.util.List;
+
 /**
  * 徐大伟2019.8.21
  * 批次发货
@@ -16,5 +18,8 @@ public interface FoodManageService1 {
      * id查询批次
      */
     public Batch queryBatch(String batchBarcode);
-
+    /**
+     * 查询所有批次
+     */
+    public List<Batch> queryall();
 }
