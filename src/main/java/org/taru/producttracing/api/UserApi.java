febbca9521f420203cc39b1bc2f0cdd0fe47c172
zyx <zyx1020d@163.com>
2019-08-25 11:51:04 +0800

    工厂 update

--- src/main/java/org/taru/producttracing/api/UserApi.java
+++ src/main/java/org/taru/producttracing/api/UserApi.java
@@ -60,7 +60,7 @@ public class UserApi {
     @RequestMapping("/api/addfactory")
     public JsonResult addFactory(Factory factory) throws ParseException {
 
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");//设置日期格式
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
         df.format(new Date());// new Date()为获取当前系统时间
           factory.setFactoryRegisterTime(df.format(new Date()));
 
