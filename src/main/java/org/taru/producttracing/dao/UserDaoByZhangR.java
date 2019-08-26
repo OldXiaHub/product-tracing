2c4724ae03323fd0f6c9001c46de3189692301ec
Zhangrui-super <1787217893@qq.com>
2019-08-26 10:47:49 +0800

    2019/8/26-----张睿10：47前台用户投诉加了电话

--- src/main/java/org/taru/producttracing/dao/UserDaoByZhangR.java
+++ src/main/java/org/taru/producttracing/dao/UserDaoByZhangR.java
@@ -51,7 +51,7 @@ public interface UserDaoByZhangR {
      * author:zhangrui
      * time:2019/8/22-21:10
      */
-    public int complain(@Param("complainId")String complainId,@Param("complainName") String complainName,@Param("complainContent") String complainContent,@Param("complainUserId") String complainUserId,@Param("complainTime") String complainTime);
+    public int complain(@Param("complainId")String complainId,@Param("complainName") String complainName,@Param("complainContent") String complainContent,@Param("complainUserId") String complainUserId,@Param("complainTime") String complainTime,@Param("complainUserTelphone") String complainUserTelphone);
 
     /**
      * 查询所有用户信息
