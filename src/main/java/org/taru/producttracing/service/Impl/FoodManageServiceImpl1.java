ce661ba74439e735d2ae06002af69bf7ae37e6f5
helloDawei <759843425@qq.com>
2019-08-27 09:33:38 +0800

    2019.8.26 徐大伟

--- src/main/java/org/taru/producttracing/service/Impl/FoodManageServiceImpl1.java
+++ src/main/java/org/taru/producttracing/service/Impl/FoodManageServiceImpl1.java
@@ -41,6 +41,9 @@ public class FoodManageServiceImpl1 implements FoodManageService1 {
         batch.setBatchTime(s);
         String batchId = IdUtil.getDateId();
         batch.setBatchId(batchId);
+        int i=1;
+        batch.setBatchStatus(i);
+        batch.setBatchProductId(IdUtil.getUuid());
         brcode=batch.getBatchQrcode();
         foodManageDao1.sendGoods(batch);
         QRFactory.creteQRFile(batch.getBatchBarcode());
