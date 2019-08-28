78bb880bebec1476b2ce3e6c938d81972ed98b32
OldXiaHub <799532619@qq.com>
2019-08-28 13:01:30 +0800

    20180826 22:00 批次实体

--- src/main/java/org/taru/producttracing/api/NewsApi.java
+++ src/main/java/org/taru/producttracing/api/NewsApi.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.taru.producttracing.pojo.Complain;
 import org.taru.producttracing.pojo.News;
 import org.taru.producttracing.service.Impl.NewsServiceImpl;
 import org.taru.producttracing.vo.JsonResult;
@@ -21,7 +22,7 @@ public class NewsApi {
      */
     @Autowired
     NewsServiceImpl serviceImpl;
-    @RequestMapping(value = "/api/adminews/selectApi")
+    @RequestMapping(value = "/api/adminnews/selectApi")
     public JsonResult queryNews(Integer pageNum,Integer pageSize){
         JsonResult  result=null;
        try {
@@ -44,7 +45,7 @@ public class NewsApi {
      * @param newsId
      * @return
      */
-    @RequestMapping(value = "/api/adminews/delnews")
+    @RequestMapping(value = "/api/adminnews/delnews")
     public JsonResult deleteNews(String newsId) {
         JsonResult  result=null;
         try {
@@ -63,7 +64,7 @@ public class NewsApi {
     /**
      * 根据id查询新闻
      */
-    @RequestMapping(value = "/api/adminews/selectNewsById")
+    @RequestMapping(value = "/api/adminnews/selectNewsById")
     public JsonResult selectNewsById(String newsId) {
         JsonResult  result=null;
         try {
@@ -85,7 +86,7 @@ public class NewsApi {
      * @param pageSize
      * @return
      */
-    @RequestMapping(value = "/api/adminews/sortByTime")
+    @RequestMapping(value = "/api/adminnews/sortByTime")
     public JsonResult sortByTime(Integer pageNum,Integer pageSize){
         JsonResult  result=null;
         try {
@@ -104,4 +105,46 @@ public class NewsApi {
         return result;
     }
 
+    /*发布新闻*/
+    @RequestMapping(value = "/api/adminnews/insertNews")
+    public JsonResult insertNews(News news){
+        JsonResult result=null;
+        if (news!=null) {
+            try {
+                serviceImpl.insertNews(news);
+                result = new JsonResult("200", "发布新闻成功", null);
+            } catch (Exception e) {
+                e.printStackTrace();
+                result = new JsonResult("500", "发布新闻失败", null);
+            }
+        }else {
+            result = new JsonResult("404", "未输入", null);
+        }
+
+        return result;
+    }
+
+    /**
+     * 新闻模糊查询(带分页)
+     * @param newsName
+     * @return
+     */
+    @RequestMapping("api/adminnews/querynewslike")
+    public JsonResult queryNewsLike(String newsName,Integer pageNum,Integer pageSize){
+        JsonResult jsonResult=null;
+        if(newsName!=null){
+            try{
+                PageHelper.startPage(pageNum,pageSize);
+                List<News>  list= serviceImpl.queryNewsLike(newsName);
+                PageInfo pageinfo=new PageInfo(list);
+                jsonResult=new JsonResult("200","查询成功",pageinfo);
+            }catch(Exception e){
+                e.printStackTrace();
+                jsonResult=new JsonResult("500",e.getMessage(),"");
+            }
+        }else{
+            jsonResult=new JsonResult("404","查询失败","");
+        }
+        return jsonResult;
+    }
 }
--- src/main/java/org/taru/producttracing/api/PhotoApi.java
+++ src/main/java/org/taru/producttracing/api/PhotoApi.java
@@ -17,18 +17,18 @@ import static java.util.UUID.randomUUID;
 @RestController
 @CrossOrigin()
 public class PhotoApi {
-    @Value("${web.upload.imagepath}") //大括号里的都是key值   $表示调用配置文件     #表示调用xml配置文件
+    @Value("${web.upload.image-path}") //大括号里的都是key值   $表示调用配置文件     #表示调用xml配置文件
     private String imagepath;//动态路径
 
 
     @RequestMapping("/api/user/upload")
-    public JsonResult upload(@RequestParam("image-header") MultipartFile multipartFile){
+    public JsonResult upload(@RequestParam("image_header") MultipartFile multipartFile){
         JsonResult result=null;
         String []a=multipartFile.getOriginalFilename().split("\\.");//截取图片后缀名
         if(!multipartFile.isEmpty()){
             try {
-                String filename= UUID.randomUUID().toString().replace("-", "");
-                multipartFile.transferTo(new File(imagepath+"\\"+filename+"."+a[1]));
+                String filename= UUID.randomUUID().toString().replace("-", "")+"."+a[1];
+                multipartFile.transferTo(new File(imagepath+"\\"+filename));
                 result=new JsonResult("200","上传图片成功",filename);
             }catch (Exception e){
                 e.printStackTrace();
